0e7ec1f9959878258d42064b850bcb6240ebe20a
CG <cgodlew@gmail.com>
2020-06-14 01:01:53 -0400

    Updated user.js file with authentication process

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+// mongodb+srv://<dev>:<gz5mu6KPX2eRVpU5>@cluster0-vas4z.mongodb.net/<dbname>?retryWrites=true&w=majority
 
 app.get("/", (req, res) => res.send("This is yet another message"));
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
